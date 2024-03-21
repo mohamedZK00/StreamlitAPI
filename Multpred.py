e7698dbc4fcd3bf4f8316f101040f1575f82302a
mohamedZK00 <163613527+mohamedZK00@users.noreply.github.com>
2024-03-21 19:04:17 +0200

    Update Multpred.py

--- Multpred.py
+++ Multpred.py
@@ -6,9 +6,9 @@ from streamlit_option_menu import option_menu
 #Model_1 = pickle.load(open('mdel87%.PLK'))
 
 # فتح الملف بنمط 'rb' (قراءة بنمط ثنائي)
-with open('mdel87%.PLK', 'rb') as file:
-    Model_1 = pickle.load(file)
-
+#with open('mdel87%.PLK', 'rb') as file:
+  #  Model_1 = pickle.load(file)
+ Model_1 = pickle.load('Streamlit/mdel87%.PLK')
 # Classification Model
 #Model_2 = pickle.load(open('B:\\ML-Streamlit\\GBRmodel_99%_3.PLK', 'rb'))
 

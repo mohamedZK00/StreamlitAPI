47ca72797f7e8ffd3636dc4007c713e7a1ff1abd
mohamedZK00 <163613527+mohamedZK00@users.noreply.github.com>
2024-03-22 23:13:16 +0200

    Update Multpred.py

--- Multpred.py
+++ Multpred.py
@@ -13,7 +13,7 @@ from streamlit_option_menu import option_menu
   #  Model_1 = pickle.load(file)
 
 #working_dir = os.path.dirname(os.path.abspath(__file__))
-Model_1 =pickle.load(open('Saved_model//mdel87%.PLK','rb'))
+Model_1 =pickle.load(open('Saved_model:/mdel87%.PLK','rb'))
 
 
 #Model_1 =pickle.load(open('Saved_model/mdel87%.PLK','rb'))

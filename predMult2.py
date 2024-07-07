22732248b87a89b2abcf8ce4642605d1c3aa9f9b
mohamedZK00 <163613527+mohamedZK00@users.noreply.github.com>
2024-07-08 01:58:14 +0300

    Update predMult2.py

--- predMult2.py
+++ predMult2.py
@@ -23,6 +23,86 @@ with st.sidebar:
     selected = option_menu('Multiple Student Grade Prediction System', options, icons=icons, default_index=0)
 
 # Define data for random grades
+
+
+# Predict student grades
+if selected == 'Predict Student Grades':
+    # Page title
+    st.title('Predict Student Grades using ML')
+
+    grade_month_1 = st.text_input('degree of Mo.1')
+    grade_month_2 = st.text_input('degree of Mo.2')
+    grade_month_3 = st.text_input('degree of Mo.3')
+
+    # Code for prediction
+    pred_Grd = ''
+
+    # Creating a button for prediction
+    if st.button('Grade prediction'):
+        grd_pred = Model.predict([[grade_month_1, grade_month_2, grade_month_3]])
+
+        predicted_grade = {"grd_pred": round(grd_pred[0], 1)}
+        max_grade = 100
+        predicted_percentage = round((predicted_grade["grd_pred"] / max_grade) * 100, 1)
+        formatted_result = "Student grade : {:.1f}%".format(predicted_percentage)
+
+        st.success(formatted_result)
+
+# Classification of student grades
+if selected == 'Classification of Student Grades':
+    # Page title
+    st.title('Classification of Student Grades using ML')
+
+    grade_month_1 = st.text_input('degree of Mo.1')
+    grade_month_2 = st.text_input('degree of Mo.2')
+    grade_month_3 = st.text_input('degree of Mo.3')
+
+    # Code for prediction
+    Cls_Grd = ''
+
+    # Creating a button for prediction
+    if st.button('Grade Classification '):
+        ls_pred = Model.predict([[grade_month_1, grade_month_2, grade_month_3]])
+
+        def classify_grade(grade):
+            if grade >= 90:
+                return 'A'
+            elif grade >= 80:
+                return 'B'
+            elif grade >= 70:
+                return 'C'
+            elif grade >= 60:
+                return 'D'
+            elif grade >= 50:
+                return 'E'
+            else:
+                return 'F'
+
+        classified_grades = [classify_grade(grade) for grade in ls_pred]
+        st.success(classified_grades)
+
+        def classify_grade(grade):
+            if grade == 'A':
+                return 'Excellent'
+            elif grade == 'B':
+                return 'Very Good'
+            elif grade == 'C':
+                return 'Good'
+            elif grade == 'D':
+                return 'Satisfactory'
+            elif grade == 'E':
+                return 'Sufficient'
+            else:
+                return 'Fail'
+
+        clas_grades = [classify_grade(grade) for grade in classified_grades]
+        st.success(clas_grades)
+
+
+
+
+
+
 num_students = 1000
 np.random.seed(42)
 grades_month1 = np.random.randint(40, 75, num_students)
@@ -140,76 +220,3 @@ if st.button('Predict'):
 
         # Show plot in Streamlit
         st.pyplot(fig)
-
-# Predict student grades
-if selected == 'Predict Student Grades':
-    # Page title
-    st.title('Predict Student Grades using ML')
-
-    grade_month_1 = st.text_input('degree of Mo.1')
-    grade_month_2 = st.text_input('degree of Mo.2')
-    grade_month_3 = st.text_input('degree of Mo.3')
-
-    # Code for prediction
-    pred_Grd = ''
-
-    # Creating a button for prediction
-    if st.button('Grade prediction'):
-        grd_pred = Model.predict([[grade_month_1, grade_month_2, grade_month_3]])
-
-        predicted_grade = {"grd_pred": round(grd_pred[0], 1)}
-        max_grade = 100
-        predicted_percentage = round((predicted_grade["grd_pred"] / max_grade) * 100, 1)
-        formatted_result = "Student grade : {:.1f}%".format(predicted_percentage)
-
-        st.success(formatted_result)
-
-# Classification of student grades
-if selected == 'Classification of Student Grades':
-    # Page title
-    st.title('Classification of Student Grades using ML')
-
-    grade_month_1 = st.text_input('degree of Mo.1')
-    grade_month_2 = st.text_input('degree of Mo.2')
-    grade_month_3 = st.text_input('degree of Mo.3')
-
-    # Code for prediction
-    Cls_Grd = ''
-
-    # Creating a button for prediction
-    if st.button('Grade Classification '):
-        ls_pred = Model.predict([[grade_month_1, grade_month_2, grade_month_3]])
-
-        def classify_grade(grade):
-            if grade >= 90:
-                return 'A'
-            elif grade >= 80:
-                return 'B'
-            elif grade >= 70:
-                return 'C'
-            elif grade >= 60:
-                return 'D'
-            elif grade >= 50:
-                return 'E'
-            else:
-                return 'F'
-
-        classified_grades = [classify_grade(grade) for grade in ls_pred]
-        st.success(classified_grades)
-
-        def classify_grade(grade):
-            if grade == 'A':
-                return 'Excellent'
-            elif grade == 'B':
-                return 'Very Good'
-            elif grade == 'C':
-                return 'Good'
-            elif grade == 'D':
-                return 'Satisfactory'
-            elif grade == 'E':
-                return 'Sufficient'
-            else:
-                return 'Fail'
-
-        clas_grades = [classify_grade(grade) for grade in classified_grades]
-        st.success(clas_grades)
